068dae0c2d90cf0dc1674d3e8ca1b5c7f6d232dd
VladimirVocniak <vozniak.exceedteam@gmail.com>
2022-02-02 17:49:23 +0300

    commit

--- src/Components/TodoList/index.tsx
+++ src/Components/TodoList/index.tsx
@@ -1,29 +1,57 @@
-import React, {FC} from "react";
-import {deleteTodo, updateCompletedTodo, updateTextTodo} from "../../Redux/todoSlice";
+import React, {FC, useState} from "react";
+import {checkAllTodos, deleteTodo, updateCompletedTodo, updateTextTodo} from "../../Redux/todoSlice";
 import {ITodo} from "../../intefaces";
 import {useDispatch} from "react-redux";
+import "./style.css";
 
 interface ITodoList {
     todos: ITodo[]
 }
 
+// todos.length === todos.filter(elem => elem.completed).length
+
 export const TodoList: FC<ITodoList> = ({todos}) => {
+    // let [checkAll, setCheckAll] = useState<boolean>(false);
+    let checkAlll: boolean = todos.filter(elem => elem.completed).length === todos.length;
     const dispatch = useDispatch();
+    console.log(checkAlll);
+
+    const numberOfChecked = (): boolean => {
+        const checkedTodos = todos.filter(elem => elem.completed).length;
+        return checkedTodos === 0 || checkedTodos === todos.length;
+    };
+
+    const checkAllTodo = (checkAll: boolean): void => {
+        dispatch(checkAllTodos(checkAll));
+        checkAlll = checkAll;
+    };
 
     return (
-        <div className="todos-list">
-            {todos.length ? todos.map((elem) =>
-                <div key={elem.id}>
-                    <input type="checkbox" checked={elem.completed}
-                           onChange={checked => dispatch(updateCompletedTodo({
-                               id: elem.id,
-                               completed: checked.target.checked
-                           }))}/>
-                    <input value={elem.text} disabled={elem.completed}
-                           onChange={e => dispatch(updateTextTodo({id: elem.id, text: e.target.value}))}/>
-                    <button onClick={() => dispatch(deleteTodo(elem.id))}>X</button>
+        <>
+            {todos.length ?
+                <div className="todos-list">
+                    {todos.length && todos.map((elem) =>
+                        <div className="todo-container" key={elem.id}>
+                            <input type="checkbox" checked={elem.completed}
+                                   onChange={checked => dispatch(updateCompletedTodo({
+                                       id: elem.id,
+                                       completed: checked.target.checked
+                                   }))}/>
+                            <input value={elem.text} disabled={elem.completed}
+                                   onChange={e => dispatch(updateTextTodo({id: elem.id, text: e.target.value}))}/>
+                            <button onClick={() => dispatch(deleteTodo(elem.id))}>X</button>
+                        </div>
+                    )}
+                    <label className="checkAll">
+                        <input type="checkbox" checked={checkAlll}
+                               onChange={e => checkAllTodo(e.target.checked)}/>
+                        <div className={`someCheck ${numberOfChecked() ? "someCheckOff" : ""}`}/>
+                        Check all
+                    </label>
                 </div>
-            ) : <p>No todos</p>}
-        </div>
+                :
+                <p>No todos</p>
+            }
+        </>
     );
 };
--- src/Redux/todoSlice.ts
+++ src/Redux/todoSlice.ts
@@ -7,7 +7,28 @@ interface todoSliceState {
 }
 
 const initialState: todoSliceState = {
-    todos: []
+    todos: [
+        {
+            "id": 1643805319214,
+            "text": "5",
+            "completed": false
+        },
+        {
+            "id": 1643805669337,
+            "text": "234",
+            "completed": true
+        },
+        {
+            "id": 1643805670647,
+            "text": "444",
+            "completed": false
+        },
+        {
+            "id": 1643805672479,
+            "text": "1das",
+            "completed": true
+        }
+    ]
 };
 
 export const todoSlice = createSlice({
@@ -34,12 +55,17 @@ export const todoSlice = createSlice({
                 ...elem, completed: action.payload.completed
             } : elem);
         },
+        checkAllTodos(state, action: PayloadAction<boolean>) {
+            state.todos = state.todos.map(elem => ({
+            ...elem, completed: action.payload
+            }));
+        },
         deleteTodo(state, action: PayloadAction<number>) {
             state.todos = state.todos.filter(elem => elem.id !== action.payload);
         }
     }
 });
 
-export const {addTodo, updateTextTodo, updateCompletedTodo, deleteTodo} = todoSlice.actions;
+export const {addTodo, updateTextTodo, updateCompletedTodo,checkAllTodos, deleteTodo} = todoSlice.actions;
 export const selectTodos = (state: RootState) => state.todos.todos;
 export default todoSlice.reducer;
