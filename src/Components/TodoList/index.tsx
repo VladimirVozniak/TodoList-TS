4b2bf406857bf8b0f95c2f2ac8f1c131d8e8a79b
VladimirVocniak <vozniak.exceedteam@gmail.com>
2022-02-02 18:14:52 +0300

    commit

--- src/Components/TodoList/index.tsx
+++ src/Components/TodoList/index.tsx
@@ -8,28 +8,32 @@ interface ITodoList {
     todos: ITodo[]
 }
 
-// todos.length === todos.filter(elem => elem.completed).length
-
 export const TodoList: FC<ITodoList> = ({todos}) => {
-    // let [checkAll, setCheckAll] = useState<boolean>(false);
-    let checkAlll: boolean = todos.filter(elem => elem.completed).length === todos.length;
+    // const [checkAll, setCheckAll] = useState<boolean>(false);
+    let checkAll: boolean = todos.filter(elem => elem.completed).length === todos.length;
     const dispatch = useDispatch();
-    console.log(checkAlll);
+    console.log(checkAll);
 
     const numberOfChecked = (): boolean => {
         const checkedTodos = todos.filter(elem => elem.completed).length;
         return checkedTodos === 0 || checkedTodos === todos.length;
     };
 
-    const checkAllTodo = (checkAll: boolean): void => {
-        dispatch(checkAllTodos(checkAll));
-        checkAlll = checkAll;
+    const checkAllTodo = (checked: boolean): void => {
+        dispatch(checkAllTodos(checked));
+        checkAll = checked;
     };
 
     return (
         <>
             {todos.length ?
                 <div className="todos-list">
+                    <label className="checkAll">
+                        <input type="checkbox" checked={checkAll}
+                               onChange={e => checkAllTodo(e.target.checked)}/>
+                        <div className={`someCheck ${numberOfChecked() ? "someCheckOff" : ""}`}/>
+                        Check all
+                    </label>
                     {todos.length && todos.map((elem) =>
                         <div className="todo-container" key={elem.id}>
                             <input type="checkbox" checked={elem.completed}
@@ -42,12 +46,6 @@ export const TodoList: FC<ITodoList> = ({todos}) => {
                             <button onClick={() => dispatch(deleteTodo(elem.id))}>X</button>
                         </div>
                     )}
-                    <label className="checkAll">
-                        <input type="checkbox" checked={checkAlll}
-                               onChange={e => checkAllTodo(e.target.checked)}/>
-                        <div className={`someCheck ${numberOfChecked() ? "someCheckOff" : ""}`}/>
-                        Check all
-                    </label>
                 </div>
                 :
                 <p>No todos</p>
